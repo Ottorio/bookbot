5b4642f4fd84b8410406cbc821ee6a2611debad3
Ottorio <andrzej.gawron@interia.eu>
2024-07-01 07:31:23 +0200

    add count_words

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 # def main():
 #     book_path = "books/frankenstein.txt"
-#     text = get_book_text(book_path)
-#     print(text)
+#     with open(book_path) as f:
+#         file_contents = f.read()
+#     print(file_contents)
 
-# def get_book_text(path):
-#     with open(path) as f:
-#         return f.read()
-    
 def main():
     book_path = "books/frankenstein.txt"
-    with open(book_path) as f:
-        file_contents = f.read()
-    print(file_contents)
+    text = get_book_text(book_path)
+    print(text)
+    words = count_words(text)
+    print(f"This book contains {words} words.")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+def count_words(text):
+    words = text.split()
+    return len(words)
 
 main()
