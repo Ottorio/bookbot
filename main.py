5e1aacf6f7230c159f3fc24f453865ffe05629a2
Ottorio <andrzej.gawron@interia.eu>
2024-07-02 06:56:49 +0200

    add count characters

--- main.py
+++ main.py
@@ -7,9 +7,11 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
     words = count_words(text)
+    chars_dict = count_characters(text)
     print(f"This book contains {words} words.")
+    print("Occurence of each character:")
+    print(chars_dict)
 
 def get_book_text(path):
     with open(path) as f:
@@ -19,4 +21,15 @@ def count_words(text):
     words = text.split()
     return len(words)
 
+def count_characters(text):
+    characters = {}
+    for char in text:
+        lowered = char.lower()
+        if lowered not in characters:
+            characters[lowered] = 1
+        else:
+            characters[lowered] += 1
+    return characters
+
+    
 main()
