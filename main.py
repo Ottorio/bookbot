69b26a3b3dc0b62a561341a7438125d3ca427e35
Ottorio <andrzej.gawron@interia.eu>
2024-09-30 22:57:42 +0200

    set layout and test button

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import customtkinter as ctk # type: ignore
+import customtkinter as ctk # pip3 install customtkinter
+from tkinter import filedialog # pip3 install tk
 
 def main():
     book_path = "books/frankenstein.txt"
@@ -47,11 +48,41 @@ def dict_to_list(dict):
     sorted_list.sort(reverse=True, key=sort_on)
     return sorted_list
 
+def choose_document():
+    file_location = filedialog.askopenfile()
+    book_path_entry.delete(0, ctk.END)
+    book_path_entry.insert(0, file_location)
+
+def analyze():
+    result_label.configure(text="test", font=("Roboto", 15))
+
 # create window
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 root = ctk.CTk()
 root.title("Bookbot")
 root.geometry("500x500")
+
+# set layout
+file_text = ctk.CTkLabel(root, text="Choose the location of your document", font=("Roboto",24))
+file_text.pack(padx=10, pady=10)
+
+book_path_entry = ctk.CTkEntry(root, width=400)
+book_path_entry.pack(padx=10, pady=10)
+
+choose_file = ctk.CTkButton(root, text="Choose file", font=("Roboto", 15), command=choose_document)
+choose_file.pack(padx=10, pady=10)
+
+proceed_text = ctk.CTkLabel(root, text="Proceed with the analysis", font=("Roboto", 24))
+proceed_text.pack(padx=10, pady=(30,10))
+
+analyze_button = ctk.CTkButton(root, text="Analyze", font=("Roboto", 15), command=analyze)
+analyze_button.pack(padx=10, pady=10)
+
+result_text = ctk.CTkLabel(root, text="Results", font=("Roboto", 24))
+result_text.pack(padx=10, pady=(20, 10))
+
+result_label = ctk.CTkLabel(root, text="", font=("Roboto", 15))
+result_label.pack(padx=10, pady=10)
 
 root.mainloop()
